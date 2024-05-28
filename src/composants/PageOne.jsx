bf3b1733379d6c64dfba36cf483291a15565a4cf
Etienne Chazelle <etienne.chazelle@eolas.fr>
2024-05-28 17:10:59 +0200

    finalisation liens et ajout contenu

--- src/composants/PageOne.jsx
+++ src/composants/PageOne.jsx
@@ -109,6 +109,22 @@ function PageOne() {
   }, []);
 
   useEffect(() => {
+    const path = document.getElementById("climber-path");
+    const pathLength = path.getTotalLength();
+
+    path.style.strokeDasharray = pathLength;
+    path.style.strokeDashoffset = pathLength;
+
+    const timeline = gsap.timeline();
+
+    timeline.to(path, {
+      duration: 10,
+      strokeDashoffset: 0,
+      ease: "power2.inOut",
+    });
+
+    timeline.pause();
+
     const pageOne = pageRef.current;
     setContentVisible(false);
 
@@ -119,6 +135,9 @@ function PageOne() {
         end: "bottom top",
         scrub: true,
         pin: true,
+        onEnter: () => {
+          timeline.play();
+        },
       },
     });
 
@@ -142,6 +161,31 @@ function PageOne() {
   return (
     <div className="PageOne" ref={pageRef}>
       <div className="background-2">
+        <svg width="1920" height="1080" viewBox="0 0 1920 1080" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            id="climber-path"
+            d="M435 841.5C466.333 842.833 514 846.5 606 845C678.506 843.818 595.5 854 529 866.5C462.5 879 738.5 891 818.5 908C898.5 925 795.5 917 683.5 933.5C571.5 950 762 942.5 965 977.5C1482.5 1043.5 1493 1007 1709 1043.5C1881.8 1072.7 1920 1080 1920 1080M819 801.5C956.5 791.5 1045.5 754 998 748C950.5 742 866.5 738.5 899.5 727.5C932.5 716.5 1006 709.5 995 697.5C984 685.5 877.5 669 912.5 663.5C947.5 658 1009.5 680.5 968 657C926.5 633.5 892 623.5 919 622C946 620.5 990 612.5 964 607.5C938 602.5 900.5 599.5 919 593.5C937.5 587.5 968.5 580 947.5 574.5C926.5 569 909.5 562.5 922.5 556.5C935.5 550.5 938 537.5 927.5 521.5M1036.5 609.5C1054 603.5 1071 593 1050 591.5C1033.2 590.3 1017 589.333 1011 589M905.5 589.5C810.5 565 809 563 780 535C751 507 759.5 494 750 495.5C740.5 497 714 504 722 495.5C730 487 746 485.5 734 480C722 474.5 716 472 717.5 465C719 458 729.5 479 722 456.5C717.629 443.386 715.823 437.084 715.229 434.5C715.008 433.537 714.955 433.09 715 433M713.5 465C656.5 458.5 556 450.5 578 445.5C600 440.5 652 417.5 598.5 431.5C545 445.5 514.5 446.5 512 435.5C509.5 424.5 512 415 501.5 412.5C491 410 488 408 492.5 406C497 404 503.5 406.5 500 402C497.2 398.4 494.833 393.167 494 391C493 390 376.5 211 189.5 108.5C39.9 26.5 0.833333 2 0 0"
+            stroke="url(#paint0_linear_158_10)"
+            stroke-width="2"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_158_10"
+              x1="960.516"
+              y1="0"
+              x2="960.516"
+              y2="1080"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#185997" stop-opacity="0" />
+              <stop offset="0.357213" stop-color="#1C5C99" stop-opacity="0" />
+              <stop offset="0.362339" stop-color="#24629E" stop-opacity="0.789884" />
+              <stop offset="0.741072" stop-color="#336DA5" stop-opacity="0" />
+              <stop offset="0.807047" stop-color="#356FA6" stop-opacity="0.5" />
+              <stop offset="0.902425" stop-color="#356FA6" stop-opacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
         <img ref={imageRef} src={mountainsImage} alt="Mountains" />
       </div>
       <div className="pointers" style={{ height: imageHeight + "px" }}>
--- src/composants/PageTwo.jsx
+++ src/composants/PageTwo.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import climber from "../images/P3/climber.png";
 import "../styles/PageTwo.scss";
+import projets_data from "../data/projets";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function PageTwo() {
   const pageTwoRef = useRef(null);
+  const [projets, setProjets] = useState(projets_data.projets);
+
   useEffect(() => {
     const pageTwo = pageTwoRef.current;
     gsap.to(".background-3", {
@@ -36,6 +39,20 @@ function PageTwo() {
     <div className="PageTwo" ref={pageTwoRef}>
       <div className="background-3">
         <img src={climber} alt="Climber" />
+      </div>
+      <div className="content-2">
+        <div className="content-2-header">
+          <h2>PROJETS</h2>
+          <p>{projets.length}</p>
+        </div>
+        <div className="liste-projets">
+          {projets.map((projet, index) => (
+            <button className="btn-projet">
+              <h3>{projet.nom}</h3>
+              <p>{projet.date}</p>
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
