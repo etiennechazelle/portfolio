1e28432cceeffd3b01ba0368e25f4a651b1474f6
Etienne Chazelle <etienne.chazelle@eolas.fr>
2024-06-03 14:20:23 +0200

    finalisation avant responsive

--- src/composants/PageThree.jsx
+++ src/composants/PageThree.jsx
@@ -1,45 +1,39 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "../styles/PageThree.scss";
 import summit from "../images/P4/summit.png";
+import cv from "../data/CV_ETIENNE_CHAZELLE.pdf";
+import flags from "../images/P4/flags.gif";
 
-const flagImages = Array.from({ length: 28 }, (_, i) => require(`../images/P4/flags/flag${i + 1}.png`));
+gsap.registerPlugin(ScrollTrigger);
 
 function PageThree() {
-  const [flagDisplayedId, setFlagDisplayedId] = useState(1);
-  const flagRefs = useRef([]);
-
   useEffect(() => {
-    const totalFlags = flagImages.length;
-    flagRefs.current.forEach((flag, index) => {
-      if (flag) {
-        flag.style.display = index + 1 === flagDisplayedId ? "block" : "none";
-      }
+    gsap.to(".liens-4", {
+      scrollTrigger: {
+        trigger: ".background-4",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+      },
+      opacity: 1,
     });
-
-    const interval = setInterval(() => {
-      setFlagDisplayedId((prevId) => (prevId === totalFlags ? 1 : prevId + 1));
-    }, 75);
-
-    return () => clearInterval(interval);
-  }, [flagDisplayedId]);
+  }, []);
 
   return (
     <div className="PageThree" id="Contact">
       <div className="background-4">
         <img src={summit} alt="Summit" style={{ zIndex: "2" }} />
-        {flagImages.map((flagImage, index) => (
-          <img
-            key={index}
-            src={flagImage}
-            alt={`Flag ${index + 1}`}
-            ref={(el) => (flagRefs.current[index] = el)}
-            style={{ zIndex: "1" }}
-          />
-        ))}
+        <img src={flags} alt="Flags" style={{ zIndex: "1" }} />
       </div>
-      <div className="liens-4">
+      <div className="liens-4" style={{ opacity: 0 }}>
         <a href="https://www.linkedin.com/in/%C3%A9tienne-chazelle-18a448268/">LinkedIn</a>
         <a href="https://github.com/etiennechazelle/">GitHub</a>
+        <a href="mailto:etiennechazelle38@gmail.com">Mail</a>
+        <a href={cv} download>
+          CV
+        </a>
       </div>
     </div>
   );
