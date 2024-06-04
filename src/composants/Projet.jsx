6ccc8e276bbb98f1140cc4857ae003e1bdfbacca
Etienne Chazelle <etiennechazelle38@gmail.com>
2024-06-04 20:06:42 +0200

    ajout contenu

--- src/composants/Projet.jsx
+++ src/composants/Projet.jsx
@@ -11,18 +11,25 @@ function Projet({ projet }) {
   const projetContentRef = useRef(null);
 
   const imagesProjet = Array.from({ length: projet.nbImages }, (_, i) =>
-    require(`../images/data/projets/${projet.nom.replace(/ /g, "-").toLowerCase()}/${i + 1}.png`)
+    require(`../images/data/projets/${projet.nom
+      .replace(/ /g, "-")
+      .toLowerCase()}/${i + 1}.png`)
   );
 
   return (
     <div className="Projet">
-      <img src={image} alt={projet.title} className="header-image" />
+      <div className="header-image-container">
+        <img src={image} alt={projet.title} className="header-image" />
+      </div>
       <div className="projet-content" ref={projetContentRef}>
         <div className="projet-content-header">
           <h2 style={{ color: projet.color }}>{projet.nom}</h2>
           <h2 style={{ color: projet.color }}>{projet.date}</h2>
         </div>
-        <div className="separation" style={{ borderTop: `4px solid #E2EAFF`, margin: "1em 0px" }}></div>
+        <div
+          className="separation"
+          style={{ borderTop: `4px solid #E2EAFF`, margin: "1em 0px" }}
+        ></div>
         <div className="projet-content-techno">
           {technologies.map((techno, index) => (
             <span key={index} style={{ backgroundColor: projet.color }}>
@@ -30,15 +37,25 @@ function Projet({ projet }) {
             </span>
           ))}
         </div>
-        <div className="separation" style={{ borderTop: `4px solid #E2EAFF`, margin: "1em 0px" }}></div>
+        <div
+          className="separation"
+          style={{ borderTop: `4px solid #E2EAFF`, margin: "1em 0px" }}
+        ></div>
         <p className="projet-content-description">{projet.description}</p>
         <div className="projet-images-container">
           {imagesProjet.map((image, index) => (
-            <img key={index} src={image} alt={`Projet ${projet.nom} ${index + 1}`} />
+            <img
+              key={index}
+              src={image}
+              alt={`Projet ${projet.nom} ${index + 1}`}
+            />
           ))}
           {projet.video && (
             <video controls>
-              <source src={require(`../videos/${projet.video}`)} type="video/mp4" />
+              <source
+                src={require(`../videos/${projet.video}`)}
+                type="video/mp4"
+              />
               Your browser does not support the video tag.
             </video>
           )}
