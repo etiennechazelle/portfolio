49d9cc89c2f860f9119aeaca286bf6399787c3c2
Etienne Chazelle <etiennechazelle38@gmail.com>
2024-06-30 12:40:38 +0200

    ajout cta

--- src/composants/Home.jsx
+++ src/composants/Home.jsx
@@ -65,6 +65,19 @@ function Home() {
     };
   }, []);
 
+  // animate the scroll down div up and down
+  useEffect(() => {
+    const scrollDownDiv = document.querySelector(".scroll-down-div");
+
+    gsap.to(scrollDownDiv, {
+      y: 5,
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+    });
+  }, []);
+
   return (
     <div className="Home" ref={homeRef}>
       <Navbar />
@@ -81,6 +94,11 @@ function Home() {
       <svg width="1920" height="400" className="plane-path" style={{ opacity: 0.5 }}>
         <path className="path" d="M0,350 Q960,50 1920,150" fill="none" stroke="white" strokeWidth={2} />
       </svg>
+      <div className="scroll-down-div">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+        </svg>
+        </div>
     </div>
   );
 }
